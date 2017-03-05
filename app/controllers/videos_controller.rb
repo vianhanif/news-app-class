d7680c59b4fbbfa281ae077800d3d6f0072df17d
Alvian Rahman Hanif <alvian524@gmail.com>
2017-03-05 21:55:32 +0700

    fix redirect forms

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -35,7 +35,7 @@ class VideosController < ApplicationController
     end
     respond_to do |format|
       if @video.save
-        format.html { redirect_to @video, notice: 'Video was successfully created.' }
+        format.html { redirect_to videos_path, notice: 'Video was successfully created.' }
         format.json { render :show, status: :created, location: @video }
       else
         format.html { render :new }
@@ -49,7 +49,7 @@ class VideosController < ApplicationController
   def update
     respond_to do |format|
       if @video.update(video_params)
-        format.html { redirect_to @video, notice: 'Video was successfully updated.' }
+        format.html { redirect_to videos_path, notice: 'Video was successfully updated.' }
         format.json { render :show, status: :ok, location: @video }
       else
         format.html { render :edit }
