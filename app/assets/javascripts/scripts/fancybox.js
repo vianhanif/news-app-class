36d0277279bc1ae77eb132e3e6ab5f0236f23291
Alvian Rahman Hanif <alvian524@gmail.com>
2017-03-05 16:52:41 +0700

    fix fancy box adapter iframe

--- app/assets/javascripts/scripts/fancybox.js
+++ app/assets/javascripts/scripts/fancybox.js
@@ -14,14 +14,7 @@ function fancybox(){
 		}
 	});
   $(".various").fancybox({
-		maxWidth	: 800,
-		maxHeight	: 600,
-		fitToView	: false,
-		width		: '70%',
-		height		: '70%',
-		autoSize	: false,
-		closeClick	: false,
-		openEffect	: 'none',
-		closeEffect	: 'none'
+		openEffect	: 'elastic',
+		closeEffect	: 'elastic'
 	});
 }
